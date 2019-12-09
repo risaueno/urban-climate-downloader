3855b178baf3d7872242fb531f9d851941ff9382
Risa Ueno <risno@host292.jc.rl.ac.uk>
2019-12-09 01:33:45 +0000

    create jupyter notebooks for demonstration

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
-input("Dependencies: geopy, jupyter, netCDF4, scipy and cftime (1.0.1) will be installed on this environment. Press Enter to continue...")
+# input("Dependencies of this package will be installed on this environment. Press Enter to continue...")
 
 from setuptools import setup, find_packages
 
 setup(
     name="urban-climate-downloader",
     packages=find_packages(exclude=['*test']),
-    install_requires=['xarray',
+    install_requires=['xarray==0.11.3',
 		      'dask',
 		      'pandas',
+                      'toolz',
                       'numpy',
                       'geopy',
                       'jupyter',
